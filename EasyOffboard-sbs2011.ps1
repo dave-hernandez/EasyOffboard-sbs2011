a5dc91575e50a1e20285885c5fd3f9d6580a73ce
dave-hernandez <dhernandez@PNC-DJH2.proactiveway.local>
2015-08-20 15:06:30 -0700

    An offboarding script for sbs2011
    THIS SCRIPT WILL ONLY WORK AS INTENDED ON SBS2011
    
    This script is meant for users who are not well versed in Powershell, or
    any other command line. It allows a non-technical person to do a basic
    offboard of a user's account, by converting their mailbox to a Shared
    Mailbox to allow for better administration through the GUI, which also
    results in the user becoming disabled, preventing them from logging in.
    
    This script was written for Powershell 2.0 as it ships on sbs2011

---
+++ EasyOffboard-sbs2011.ps1
@@ -0,0 +1,24 @@
+﻿# EasyOffboard-sbs2011
+# by David Hernandez
+
+# WARNING: This script is only meant for use on sbs2011 servers
+# WARNING: This script was not tested for use on any other server platform
+
+# The following is a script that walks users through converting a user
+# mailbox to a shared mailbox, allowing the mail data to be preserved in
+# Exchange, and disabling the AD Account. While this can be done quickly
+# through the Convert-Mailbox cmdlet, this script is intended to be used by 
+# folks who have limited or no experience using the command line.
+
+Add-PSSnapin Microsoft.Exchange.Management.Powershell.E2010
+
+$trgt = Read-Host "Enter the primary email address of the user to convert"
+
+Get-Mailbox -Identity $trgt | ft Name,PrimarySMTPAddress,RecipientTypeDetails
+
+Read-Host "This will convert this user's mailbox to Shared. To continue, press 'Enter'"
+
+Set-Mailbox -Identity $trgt -Type Shared
+Get-Mailbox -Identity $trgt | ft Name,PrimarySMTPAddress,RecipientTypeDetails
+
+Read-Host "Complete!"
